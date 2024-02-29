aedfcb4584054c190291fcc8af2f0ec3cf8b03d5
prodt1811 <dariustoh18@gmail.com>
2024-02-29 20:31:11 +0800

    save

--- Acheivements.js
+++ Acheivements.js
@@ -1,7 +1,16 @@
 const carousel = document.querySelector(".carousel");
 
+let isDragging = false;
+
+const dragStart = () => {
+    isDragging = true;
+
+}
+
 const dragging = (e) => {
+    if(!isDragging) return;
     carousel.scrollLeft = e.pageX;
 }
 
+carousel.addEventListener("mousedown",isDragging)
 carousel.addEventListener("mousemove",dragging)
