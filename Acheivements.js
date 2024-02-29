31c90e2548bcbf29c99d3934f18a8b381e8870f7
prodt1811 <dariustoh18@gmail.com>
2024-02-29 20:47:28 +0800

    redp

--- Acheivements.js
+++ Acheivements.js
@@ -1,15 +1,15 @@
-const carousel = document.querySelector(".carousel");
+const carousel = document.querySelector (".carousel");
 
 let isDragging = false;
 
-const dragStart = () => {
-    isDragging = true;
+const dragStart = () =>{
+    isDragging = true
 }
 
-const dragging = (e) => {
+const dragging = (e) =>{
     if(!isDragging) return;
     carousel.scrollLeft = e.pageX;
 }
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mousedown",dragStart);
+carousel.addEventListener("mousemove", dragging)
